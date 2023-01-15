0de645b667b42a18eded19286116b93ae78e3fa9
gitt-12 <112321178+gitt-12@users.noreply.github.com>
2023-01-15 16:48:41 +0530

    Create 04_Escape_Sequence.py

---
+++ 04_Escape_Sequence.py
@@ -0,0 +1,12 @@
+#Escape Sequence
+story="Git is good. \nShe loves singing\\listening to music"
+print(story) 
+#1.To get the output in new line, use \n , 
+#2.to give backslash(\) use double backslashes(\\).
+#3. To get tab, use \t
+
+
+****OUTPUT****
+Git is good. 
+She loves singing\listening to music
+> 
