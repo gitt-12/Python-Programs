1a6d1116c44c2a0730a913de05323a86460f9dec
gitt-12 <112321178+gitt-12@users.noreply.github.com>
2023-01-15 16:09:49 +0530

    Update 03_string_functions.py

--- 03_string_functions.py
+++ 03_string_functions.py
@@ -2,14 +2,26 @@
 story="hi, This is Git. And I'm practising python program. I'm stufying in IIT (ISM), Dhanbad. That's it!"
 #string functions
 
-print(len(story)) #length of the characters in the string
+print(len(story)) #1length of the characters in the string
 
-print(story.endswith("!")) #to check if it ends with the given character
+print(story.endswith("!")) #2to check if it ends with the given character
 
-print(story.count("o")) #to have the total number of letters in the string of the particular letter. also works for combination of letters
+print(story.count("o")) #3to have the total number of letters in the string of the particular letter. also works for combination of letters
 
-print(story.capitalize()) #capitalize the first letter of the string
+print(story.capitalize()) #4capitalize the first letter of the string
 
-print(story.find("And")) #it returns the position at which the word is there in the character. if it is not there, then it returns -1.
+print(story.find("And")) #5it returns the position at which the word is there in the character. if it is not there, then it returns -1.
 
-print(story.replace("I'm", "Git")) #replace each I'm by Git(oldword by new word)
+print(story.replace("I'm", "Git")) #6replace each I'm by Git(oldword by new word)
+
+
+
+
+***OUTPUT***
+1)98
+2)True
+3)2
+4)Hi, this is git. and i'm practising python program. i'm stufying in iit (ism), dhanbad. that's it!
+5)17
+6)hi, This is Git. And Git practising python program. Git stufying in IIT (ISM), Dhanbad. That's it!
+> 
