de62f6d360a5422e2038fbedd5d76472946eccbc
gitt-12 <112321178+gitt-12@users.noreply.github.com>
2023-01-17 16:19:21 +0530

    Create 05_lists(chapter 4).py

---
+++ 05_lists(chapter 4).py
@@ -0,0 +1,43 @@
+#create a list using []
+
+a=[1,2,20,7,34,15,90]
+#print the list using the print() function
+print(a)
+
+print('\n')
+#Access using index using a[0],a[3],a[5]
+
+print(a[3])
+print('\n')
+#change the value of list using
+
+a[0]=111
+print(a)
+print('\n')
+#we can create a list with items of different types
+
+c=[45,"Git",True,7.5]
+print(c)
+
+#List slicing
+friends=["Git","Bidyu","Lilly","Swaggy","Tina","man","Raktim"]
+print(friends[0:4])
+print(friends[-4:-1])
+
+
+***OUTPUT***
+[1, 2, 20, 7, 34, 15, 90]
+
+
+7
+
+
+[111, 2, 20, 7, 34, 15, 90]
+
+['Git', 'Bidyu', 'Lilly', 'Swaggy']
+
+['Swaggy', 'Tina', 'man']
+> 
+
+[45, 'Git', True, 7.5]
+> 
