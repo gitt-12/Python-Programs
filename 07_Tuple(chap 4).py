99c2facb2adad9d70f4ec54fa37eb044f621f901
gitt-12 <112321178+gitt-12@users.noreply.github.com>
2023-01-18 15:42:16 +0530

    Create 07_Tuple(chap 4).py

---
+++ 07_Tuple(chap 4).py
@@ -0,0 +1,24 @@
+#Creating a tuple using ()
+t=(12,2,4,5)
+#printing the elements in the tupple
+print(t)
+print(t[0]) #print the 0th index of the tuple
+t2=() #empty tuple
+print(t2)
+t3=(1) #Wrong way to write a tuple with single element, won't  be printed in tuple form
+print(t3) #will print 1 only
+t4=(1,) #comma is necessary to print a tuple with single element
+print(t4)
+
+#Can not update the values of tuple
+
+
+***OUTPUT***
+
+
+(12, 2, 4, 5)
+12
+()
+1
+(1,)
+> 
