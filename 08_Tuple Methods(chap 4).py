8bad5eae0203231baf554facd8a73f59cbb2c188
gitt-12 <112321178+gitt-12@users.noreply.github.com>
2023-01-18 16:02:59 +0530

    Create 08_Tuple Methods(chap 4).py

---
+++ 08_Tuple Methods(chap 4).py
@@ -0,0 +1,8 @@
+#Creating a tuple using ()
+t=(1,2,2,4,5,1,8,20,1,45,6,1,7,2,8)
+print(t.count(1)) #gives the total number of times 1 is present in the tuple
+print(t.index(8)) #gives the index number at which 8 is present
+
+***OUTPUT***
+4
+6
