652741bd311d3a8de1731c90db0dd1194086e5b7
gitt-12 <112321178+gitt-12@users.noreply.github.com>
2023-01-18 15:25:58 +0530

    Update 06_list method(chap-4).py

--- 06_list method(chap-4).py
+++ 06_list method(chap-4).py
@@ -8,6 +8,12 @@ l1.reverse()
 print(l1)
 l1.append(45) #adds 45 at the end of the list
 print(l1)
+l1.insert(2, 544) #insert 544 in the 2nd position
+print(l1)
+l1.pop(2) #remove the element at the 2nd index
+print(l1)
+l1.remove(25) #remove 25 from the list
+print(l1)
 
 
 ***OUTPUT***
@@ -17,5 +23,8 @@ print(l1)
 
 [1, 2, 5, 7, 11, 25, 55, 63, 87, 100]
 [100, 87, 63, 55, 25, 11, 7, 5, 2, 1]
-[100, 87, 63, 55, 25, 11, 7, 5, 2, 1, 45]
+[2, 55, 7, 1, 63, 25, 100, 87, 5, 11, 45]
+[2, 55, 544, 7, 1, 63, 25, 100, 87, 5, 11]
+[2, 55, 1, 63, 25, 100, 87, 5, 11]
+[2, 55, 7, 1, 63, 100, 87, 5, 11]
 > 
